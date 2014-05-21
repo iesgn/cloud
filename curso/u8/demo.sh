e654fb92c8304c17d19920a9e1b30053b9e1f3bf
stack <stack@stack-desktop.(none)>
2014-05-21 12:46:32 +0200

    Cambio reglas

--- curso/u8/demo.sh
+++ curso/u8/demo.sh
@@ -26,23 +26,19 @@ neutron router-interface-add r1 subred1
 # Creamos dos puertos en la subred1 y en la subred2, que posteriormente conectaremos a r2
 
 PORT_ID1=$( neutron port-create red1 --fixed-ip ip_address=75.10.1.254|grep ' id '|awk '{print $4}')
-PORT_ID2=$( neutron port-create red2 --fixed-ip ip_address=75.10.2.254|grep ' id '|awk '{print $4}')
 neutron router-interface-add r2 port=$PORT_ID1
-neutron router-interface-add r2 port=$PORT_ID2
+neutron router-interface-add r2 subred2
 
 # Conectamos r3 a la red2 y a la red3
 
-neutron router-interface-add r3 subred2
+PORT_ID2=$( neutron port-create red2 --fixed-ip ip_address=75.10.2.254|grep ' id '|awk '{print $4}')
+neutron router-interface-add r3 port=$PORT_ID2
 neutron router-interface-add r3 subred3
 
-# Añadimos una ruta de encaminamiento en r2 para que poueda acceder a la red3
-neutron router-update r2 --routes type=dict list=true destination=75.10.3.0/24,nexthop=75.10.2.1
-# Añadimos una ruta de encaminamiento en r3 para que poueda acceder a la red1
-neutron router-update r3 --routes type=dict list=true destination=75.10.1.0/24,nexthop=75.10.2.254
 # Añadimos rutas al servidor DHCP de la red1 para que envíe a las intancias un camino para llegar a la red2 y a la red3.
 neutron subnet-update subred1 --host_routes type=dict list=true destination=75.10.2.0/24,nexthop=75.10.1.254 destination=75.10.3.0/24,nexthop=75.10.1.254
 # Añadimos una ruta al servidor DHCP de la red2 para que envíe a las intancias un camino para llegar a la red3.
-neutron subnet-update subred2 --host_routes type=dict list=true destination=75.10.3.0/24,nexthop=75.10.2.1 destination=75.10.1.0/24,nexthop=75.10.2.254
+neutron subnet-update subred2 --host_routes type=dict list=true destination=75.10.3.0/24,nexthop=75.10.2.254 
 
 # Creamos las reglas de seguridad (Permitimos IVMP y SSH)
 
@@ -78,3 +74,9 @@ nova boot --flavor m1.tiny2 --image ubuntu \
 
 IP=$(nova floating-ip-create|awk 'NR==4'|awk '{print $2}')
 nova floating-ip-associate pc1 $IP
+
+# Añadimos una ruta de encaminamiento en r2 para que poueda acceder a la red3
+neutron router-update r2 --routes type=dict list=true destination=75.10.3.0/24,nexthop=75.10.2.254
+# Añadimos una ruta de encaminamiento en r3 para que poueda acceder a la red1
+neutron router-update r3 --routes type=dict list=true destination=75.10.1.0/24,nexthop=75.10.2.1
+
